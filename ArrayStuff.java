d51fecde26710182f5e1f8bddf76f0acca35b575
crazytaxi <michael.jin.1729@gmail.com>
2014-10-27 09:17:07 -0400

    added array assignment

--- ArrayStuff.java
+++ ArrayStuff.java
@@ -2,6 +2,8 @@
 
 In class assignment - 100 random ints between 25 to 150 are loaded into the int array
 
+HW 14 - added find() and max val value
+
 */
 
 public class ArrayStuff {
@@ -10,5 +12,24 @@ public class ArrayStuff {
 		for (int i = 0; i < intarr.length; i++) {
 			intarr[i] = (int)(Math.ceil(Math.random() * 126.0d)) + 24;
 		}
+	}
+
+	public int find (int x) {
+		for (int i = 0; i < intarr.length; i++) {
+			if (intarr[i] == x) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	public int maxVal () {
+		int mV = intarr[0];
+		for (int i = 1; i < intarr.length; i++) {
+			if ( intarr[i] > maxVal) {
+				mV = intarr[i];		//found new value 
+			}
+		}
+		return mV;
 	}
 }
