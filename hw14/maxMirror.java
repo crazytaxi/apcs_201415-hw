5cabc4ff8c972af4fcd70152b8a8991ae7c3b378
crazytaxi <michael.jin.1729@gmail.com>
2014-10-30 21:57:33 -0400

    maxMirror complete

--- hw14/maxMirror.java
+++ hw14/maxMirror.java
@@ -7,6 +7,7 @@ public class maxMirror {
 		System.out.println(maxMirror(new int[] {1,2,3,8,9,3,2,1}));
 		System.out.println(maxMirror(new int[] {1,2,1,4}));
 		System.out.println(maxMirror(new int[] {7,1,2,9,7,2,1}));
+		System.out.println(maxMirror(new int[] {9, 1, 1, 4, 2, 1, 1, 1}));
 	}
 
 	public static int maxMirror (int[] nums) {
@@ -17,17 +18,39 @@ public class maxMirror {
 		}
 		for (int i = 0; i < nums.length; i++) {
 			//int current_sum = 0;
-			for (int j = 0; j < nums.length; j++) {
+			for (int j = 0; j < nums.length; j++) {		//shift reversed array towards the end
 				int current_sum = 0;
-				//if (i + j < nums.length) {
-				while (rn[i + j] == nums[j])
-					if (rn[i] == nums[j]) {
+				while ((i + j) < nums.length) {
+					if (rn[i + j] == nums[j]) {		//start or continue consecutive match pattern
 						++current_sum;
 					}
-				//}
+					else {				//consecutive reverse match pattern ended
+						break;
+					}
+					++j;
+				}
+				if (current_sum > max) {
+					max = current_sum;
+				}
 			}
-			if (current_sum > max) {
-				max = current_sum;
+
+			for (int j = 0; j < nums.length; j++) {		//shift reversed array towards the beginning
+				int current_sum = 0;
+				while ((j - i) < nums.length && (j - i) > -1) {
+					if (rn[j - i] == nums[j]) {		//start or continue consecutive match pattern
+						++current_sum;
+					}
+					else {				//consecutive reverse match pattern ended
+						break;
+					}
+					++j;
+					if (j >= nums.length) {
+						break;
+					}
+				}
+				if (current_sum > max) {
+					max = current_sum;
+				}
 			}
 		}
 		return max;
