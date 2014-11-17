1a914d05b4bd153d72746d6c93ef8edf36f080b2
crazytaxi <michael.jin.1729@gmail.com>
2014-11-17 09:26:41 -0500

    array copy index

--- hw17/sademo3.java
+++ hw17/sademo3.java
@@ -8,18 +8,22 @@ public class sademo3 {
 	public static void main(String[] args) {
 		ArrayList<Integer> al = new ArrayList<Integer> ();
 		SArray sa = new SArray ();
-		for (int i = 0; i < 20; i++) {
-			al.add(new Integer(i));
+		for (int i = 0; i < 400; i++) {
+			//al.add(new Integer(i));
 			sa.add(i);
 		}
-		for (int i = 0; i < 2000; i++) {
-			int index0 = (int)(Math.floor(Math.random() * 20));
-			int index1 = (int)(Math.floor(Math.random() * 20));
-			al.add(index1, al.remove(index0));
-			sa.add(index1, sa.remove(index0));
+		for (int i = 0; i < 400; i++) {
+			int index0 = (int)(Math.floor(Math.random() * 400));
+			int index1 = (int)(Math.floor(Math.random() * 400));
+			//al.add(index1, al.remove(index0));
+			sa.addcpy(index1, sa.rmcpy(index0));
 		}
 		//System.out.println(al);
 		//sa.pString();
+
+
+		//check if both arrays are the same
+		/*
 		boolean match = true;
 		for (int i = 0; i < al.size(); i++) {
 			if (!((Integer)al.get(i)).equals((Integer)sa.get(i))){
@@ -33,5 +37,6 @@ public class sademo3 {
 		else {
 			System.out.println("Arrays do no match!");
 		}
+		*/
 	}
 }
