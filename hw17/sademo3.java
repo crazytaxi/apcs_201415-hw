1746d02288bc009babc76bea5f541c4925773718
crazytaxi <michael.jin.1729@gmail.com>
2014-11-17 08:53:42 -0500

    increased array size

--- hw17/sademo3.java
+++ hw17/sademo3.java
@@ -12,13 +12,26 @@ public class sademo3 {
 			al.add(new Integer(i));
 			sa.add(i);
 		}
-		for (int i = 0; i < 400; i++) {
+		for (int i = 0; i < 2000; i++) {
 			int index0 = (int)(Math.floor(Math.random() * 20));
 			int index1 = (int)(Math.floor(Math.random() * 20));
 			al.add(index1, al.remove(index0));
 			sa.add(index1, sa.remove(index0));
 		}
-		System.out.println(al);
-		sa.pString();
+		//System.out.println(al);
+		//sa.pString();
+		boolean match = true;
+		for (int i = 0; i < al.size(); i++) {
+			if (!((Integer)al.get(i)).equals((Integer)sa.get(i))){
+				match = false;
+				break;
+			}
+		}
+		if (match) {
+			System.out.println("Arrays match!");
+		}
+		else {
+			System.out.println("Arrays do no match!");
+		}
 	}
 }
