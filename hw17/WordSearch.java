66fc73d9ef028c491b152b7999ed049d5018a240
crazytaxi <michael.jin.1729@gmail.com>
2014-11-18 09:19:09 -0500

    simple constructor for word search

---
+++ hw17/WordSearch.java
@@ -0,0 +1,10 @@
+public class WordSearch {
+	private char[][] grid;
+
+	public WordSearch (int row, int col) {
+		this.grid = new char[row][col];
+	}
+	public WordSearch () {
+		this.grid = new char[2][2];
+	}
+}
