666e152c943eae9af991c2a7bc370631e05aeb7a
crazytaxi <michael.jin.1729@gmail.com>
2014-11-26 09:06:40 -0500

    sarray for strings

---
+++ hw19/sademo5.java
@@ -0,0 +1,16 @@
+public class sademo5 {
+	public static void main(String[] args) {
+		SArray sa = new SArray();
+		sa.add("laugh");
+		sa.pString();
+		sa.add("hungry");
+		sa.pString();
+		sa.add(0,"optimal");
+		sa.pString();
+		sa.add(0,"drunk");
+		sa.pString();
+		sa.remove(0);
+		sa.pString();
+	}
+
+}
