d341f30b27ccc8f674708c7534b57e7aa62f4682
crazytaxi <michael.jin.1729@gmail.com>
2014-12-10 08:54:55 -0500

    added compareTo

--- hw21/Interval.java
+++ hw21/Interval.java
@@ -22,4 +22,28 @@ public class Interval {
 	public String toString () {
 		return new String("[" + this.l + ", " + this.h + "]");
 	}
+
+	public double getLow() {
+		return this.l;
+	}
+
+	public double getHigh() {
+		return this.h;
+	}
+
+	public int compareTo(Interval oi) {
+		double ldiff, hdiff;
+		if (this.l == oi.getLow() && this.h == oi.getHigh()) {
+			return 0;
+		}
+		else if ((ldiff = this.l - oi.getLow()) < 0) {
+			return (int)ldiff;
+		}
+		else if ((hdiff = this.h - oi.getHigh()) > 0) {
+			return (int)hdiff;
+		}
+		else {
+			return 0;
+		}
+	}
 }
